9dbcdc923b80689d7e65adbb0ccafbf7ec928781
Massimo Stefani <massimo.cruz25@gmail.com>
2024-10-30 19:11:14 +0100

    feat: yellow activity

--- app/src/main/java/ch/heigvd/iict/daa/template/MainActivity.kt
+++ app/src/main/java/ch/heigvd/iict/daa/template/MainActivity.kt
@@ -1,11 +1,31 @@
 package ch.heigvd.iict.daa.template
 
 import android.os.Bundle
+import android.view.View
+import android.widget.LinearLayout
+import android.widget.RadioGroup
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.green_activity)
+
+        val studentLayout = findViewById<LinearLayout>(R.id.student_data_layout)
+        val workerLayout = findViewById<LinearLayout>(R.id.worker_data_layout)
+        val radioGroup = findViewById<RadioGroup>(R.id.base_radio_group_occupation)
+
+        radioGroup.setOnCheckedChangeListener { _, checkedId ->
+            when (checkedId) {
+                R.id.base_radio_button_student -> {
+                    studentLayout.visibility = View.VISIBLE
+                    workerLayout.visibility = View.GONE
+                }
+                R.id.base_radio_button_worker -> {
+                    studentLayout.visibility = View.GONE
+                    workerLayout.visibility = View.VISIBLE
+                }
+            }
+        }
     }
 }
